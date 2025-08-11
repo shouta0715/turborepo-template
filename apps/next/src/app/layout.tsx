bfe4b1eec844b3a27b485ea91e19f387edf0d6bf
shouta0715 <kshouta0715@gmail.com>
2025-08-11 17:49:37 +0900

    remove: tailwind config

--- apps/next/src/app/layout.tsx
+++ apps/next/src/app/layout.tsx
@@ -1,14 +1,4 @@
-import { GeistSans as geistSans } from "geist/font/sans";
-import { Noto_Sans_JP } from "next/font/google";
-import { cn } from "@/lib/cn";
-
 import "@/styles/globals.css";
-
-const notoSans = Noto_Sans_JP({
-  subsets: ["latin"],
-  variable: "--font-noto-sans",
-  display: "swap",
-});
 
 type Props = {
   children: React.ReactNode;
@@ -16,12 +6,8 @@ type Props = {
 
 export default async function RootLayout({ children }: Props) {
   return (
-    <html
-      suppressHydrationWarning
-      className={cn(geistSans.variable, notoSans.variable, "font-noto-sans")}
-      lang="ja"
-    >
-      <body className="bg-background-100 text-gray-1000">{children}</body>
+    <html suppressHydrationWarning lang="ja">
+      <body>{children}</body>
     </html>
   );
 }
