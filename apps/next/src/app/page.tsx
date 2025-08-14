6ce945da8e9003ed7d83b97d7c9df8f1b687eb16
shouta0715 <kshouta0715@gmail.com>
2025-08-14 21:36:04 +0900

    chore

--- apps/next/src/app/page.tsx
+++ apps/next/src/app/page.tsx
@@ -1,5 +1,9 @@
 import { NextSegmentPage } from "next";
 
-const Page: NextSegmentPage = () => <main>Hello</main>;
+const Page: NextSegmentPage = () => {
+  const a = 1;
+
+  return <main>Hello</main>;
+};
 
 export default Page;
