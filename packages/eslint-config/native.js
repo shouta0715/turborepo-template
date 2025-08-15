61a52575cae8a0044779a990b5b01d1200aed712
shouta0715 <kshouta0715@gmail.com>
2025-08-15 15:28:59 +0900

    add: expo app

---
+++ packages/eslint-config/native.js
@@ -0,0 +1,21 @@
+import tseslint from "typescript-eslint";
+import tsConfig from "./typescript.js";
+import reactConfig from "./react.js";
+import eslintPluginN from "eslint-plugin-n";
+import expoPlugin from "eslint-plugin-expo";
+
+const config = tseslint.config(...tsConfig, ...reactConfig, {
+  name: "native",
+  files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+  plugins: {
+    "eslint-plugin-n": eslintPluginN,
+    expo: expoPlugin,
+  },
+  rules: {
+    "eslint-plugin-n/no-process-env": "error",
+    "expo/no-env-var-destructuring": "error",
+    "expo/no-dynamic-env-var": "error",
+  },
+});
+
+export default config;
