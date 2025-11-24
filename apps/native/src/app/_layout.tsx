65050a694a0ca7af5863221a146a6e9d58850559
shouta0715 <kshouta0715@gmail.com>
2025-11-24 15:52:37 +0900

    install nativewind

--- apps/native/src/app/_layout.tsx
+++ apps/native/src/app/_layout.tsx
@@ -1,4 +1,5 @@
 import { Stack } from "expo-router";
+import "@/styles/tailwind.css";
 
 export default function RootLayout() {
   return <Stack />;
---
+++ apps/native/src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-xl font-bold text-blue-600">Hello World</Text>
+    </View>
+  );
+}
