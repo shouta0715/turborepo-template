6582920ef63875888d4c5fb3678a735f15fd40c7
shouta0715 <kshouta0715@gmail.com>
2025-12-14 10:21:46 +0900

    feat: hero ui

--- apps/native/src/app/index.tsx
+++ apps/native/src/app/index.tsx
@@ -1,9 +1,12 @@
-import { Text, View } from "react-native";
+import { Button } from "heroui-native";
+import { View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-xl font-bold text-blue-600">Hello World</Text>
+      <Button size="sm" variant="primary">
+        <Button.Label>Sign Up</Button.Label>
+      </Button>
     </View>
   );
 }
